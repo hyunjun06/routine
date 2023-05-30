af4d30554063b545bee9d8592a69c2f1ba935dde
hyunjun06 <junkayden@gmail.com>
2023-05-30 11:45:03 +0900

    Added animation for routine type

--- lib/screens/widgets/routine_page.dart
+++ lib/screens/widgets/routine_page.dart
@@ -16,8 +16,22 @@ class RoutinePage extends StatelessWidget {
         children: const [
           RoutineType(),
           RoutineName(),
+          RoutineDate(),
         ],
       ),
+    );
+  }
+}
+
+class RoutineDate extends StatelessWidget {
+  const RoutineDate({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: const [],
     );
   }
 }
@@ -58,24 +72,66 @@ class RoutineName extends StatelessWidget {
   }
 }
 
-class RoutineType extends StatelessWidget {
+class RoutineType extends StatefulWidget {
   const RoutineType({
     super.key,
   });
 
   @override
+  State<RoutineType> createState() => _RoutineTypeState();
+}
+
+class _RoutineTypeState extends State<RoutineType>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _animationController;
+  final _typeList = ["상체", "하체", "코어", "전신"];
+  int _typeIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 500),
+      lowerBound: 0.25,
+      upperBound: 1,
+    )
+      ..stop()
+      ..value = 1;
+  }
+
+  void _nextIndex() {
+    _animationController
+      ..stop()
+      ..value = 0.25
+      ..forward();
+    setState(() {
+      _typeIndex = (_typeIndex + 1) % _typeList.length;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      width: Sizes.size72,
-      height: Sizes.size28,
-      decoration: BoxDecoration(
-        color: Colors.black.withAlpha(30),
-        borderRadius: BorderRadius.circular(Sizes.size6),
-      ),
-      child: const Center(
-        child: Text(
-          "상체",
-          style: TextStyle(color: Colors.white),
+    return GestureDetector(
+      onTap: _nextIndex,
+      child: ScaleTransition(
+        scale: CurvedAnimation(
+          parent: _animationController,
+          curve: Curves.elasticOut,
+        ),
+        child: Container(
+          width: Sizes.size72,
+          height: Sizes.size28,
+          decoration: BoxDecoration(
+            color: Colors.black.withAlpha(30),
+            borderRadius: BorderRadius.circular(Sizes.size6),
+          ),
+          child: Center(
+            child: Text(
+              _typeList[_typeIndex],
+              style: const TextStyle(color: Colors.white),
+            ),
+          ),
         ),
       ),
     );
