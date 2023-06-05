36e9df6e7989285f9c7a77c9a1a6eb827b860d42
hyunjun06 <junkayden@gmail.com>
2023-06-05 13:42:54 +0900

    Adding new page to the app

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -2,17 +2,53 @@ import 'package:flutter/material.dart';
 import 'package:routine/constants/sizes.dart';
 import 'package:routine/screens/widgets/routine_page.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  late PageController _pageController;
+  final List<Widget> _pages = [
+    const RoutinePage(),
+    const RoutinePage(),
+    const RoutinePage()
+  ];
+  int _currentIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController(initialPage: _currentIndex);
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  void _addPage() {
+    setState(() {
+      _pages.add(const RoutinePage());
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: [
-          PageView.builder(itemBuilder: (context, index) {
-            return const RoutinePage();
-          }),
+          PageView.builder(
+            controller: _pageController,
+            itemCount: _pages.length,
+            onPageChanged: (index) => setState(() => _currentIndex = index),
+            itemBuilder: (context, index) {
+              return _pages[index];
+            },
+          ),
           SafeArea(
             child: Align(
               alignment: Alignment.topCenter,
